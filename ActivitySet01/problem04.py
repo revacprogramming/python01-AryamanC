fd045920b791a2a04e98263cb4a7f8feb976adfb
AryamanC <76692650+AryamanC@users.noreply.github.com>
2022-05-12 19:49:22 +0530

    Update problem04.py

--- ActivitySet01/problem04.py
+++ ActivitySet01/problem04.py
@@ -1,3 +1,11 @@
 # Conditional Execution
 
-hrs = input("Enter hours? ")
+hrs = float(input("Enter hours?"))
+rph = float(input("enter rate"))
+if(hrs<=40):
+  gp= rph*hrs
+  print("gross pay is",gp)
+elif(hrs>40):
+  gp=((rph*40)+(1.5*rph*(hrs-40)))
+  print("gross pay",gp)
+
